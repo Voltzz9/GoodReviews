c13445ebe30a48fe9c614d6f3a7fabba06c49770
Matthew Holm <26067404@sun.ac.za>
2024-09-30 21:28:59 +0200

    Added scraper that collects the top 50 most shelved books (i.e. most popular) and stores them in a .csv file.

---
+++ WebScrapingExplore/all_books_scraper.R
@@ -0,0 +1,51 @@
+# Load required libraries
+library(rvest)
+library(tidyverse)
+library(stringr)
+
+rm(list = ls())
+# URL of the product page
+main_vector <- c()
+
+front_page_url <- "https://www.goodreads.com"
+
+
+headers <- c(
+  "User-Agent" = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+  "Accept-Language" = "en-US,en;q=0.5"
+)
+
+page <- read_html(front_page_url, headers = headers)
+
+genre_links <- page %>%
+  html_nodes("#browseBox .gr-hyperlink") %>%
+  html_attr("href")
+
+genre_names <- genre_links[grep("^/genres/", genre_links)]
+genre_names <- sub("^/genres/", "", genre_names)
+
+full_shelf_links <- paste0("https://www.goodreads.com/shelf/show/", genre_names)
+
+full_shelf_links[4] <- "https://www.goodreads.com/shelf/show/childrens"
+
+# view(full_shelf_links)
+
+for (shelf_link in full_shelf_links) {
+  
+  shelf_page <- read_html(shelf_link, headers = headers)
+  
+  book_links <- shelf_page %>%
+    html_nodes(".bookTitle") %>%
+    html_attr("href")
+  
+  full_book_links <- paste0(front_page_url, book_links)
+  main_vector <- c(main_vector, full_book_links)
+  
+}
+
+view(main_vector)
+
+book_links_df <- data.frame(BookLinks = main_vector)
+
+view(book_links_df)
+write.csv(book_links_df, "book_links_all.csv", row.names = FALSE)
